f0a8d630d9b018efc78142e4b5136ad9f59dc275
koi141 <koichiro.ishii@oracle.com>
2026-06-28 11:00:26 +0900

    vault backup: 2026-06-28 11:00:26

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "koin3z.github.io/blog",
+    baseUrl: "koin3z.github.io/notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
